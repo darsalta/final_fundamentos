d700a45789cd965fafe8eae98b8239666e88fce0
Marcel Valdez Orozco <marcel.valdez@live.com>
2013-05-01 11:42:19 -0500

    Refactor Heuristica.java.

--- HiperHeuristica/src/hiper/Heuristica.java
+++ HiperHeuristica/src/hiper/Heuristica.java
@@ -17,7 +17,7 @@ public class Heuristica {
   /**
    * Implementa DJD. REFACTOR: Needs simplification and method extraction.
    */
-  public List<PieceContainer> DJD(
+  public static List<PieceContainer> DJD(
           PieceList inputPieces,
           int widthPieceContainer,
           int heightPieceContainer,
@@ -36,13 +36,193 @@ public class Heuristica {
               containers,
               widthPieceContainer,
               heightPieceContainer);
-      /// Fills the container with the minimum pieces to fill the initialCapacity
+      /// Fills the container with the least pieces to fill the initialCapacity
       placeWithInitialCapacity(inputPieces, currentContainer, initialCapacity);
       /// Fills the remaining space with best fit of pieces possible.
       fillPieceContainerRemainder(inputPieces, currentContainer, increment);
     }
 
     return containers;
+  }
+
+  /**
+   * Fills the container with the minimum pieces to fill the initialCapacity
+   *
+   * @param descOrderPieces pieces in descending order
+   * @param container in which to place the pieces
+   * @param thresholdCapacity after which to stop placing pieces.
+   */
+  private static void placeWithInitialCapacity(
+          PieceList descOrderPieces,
+          PieceContainer container,
+          double thresholdCapacity) throws Exception {
+    /// Recorre de mayor a menor, dado que pieces está en orden DESC
+    for (Piece piece : descOrderPieces) {
+      // initialCapacity = 1/4 o 1/3
+      if (container.getUsedArea() > container.getArea() * thresholdCapacity) {
+        break;
+      }
+
+      /// If the piece fits in the container and can be placed without colision
+      if (piece.getArea() <= container.getFreeArea()
+              && tryPlaceInBottomLeft(container, piece)) {
+        container.putPiece(piece);
+      }
+    }
+    /// Remove all pieces put in the container.
+    for (Piece piece : container) {
+      descOrderPieces.remove(piece);
+    }
+  }
+
+  /**
+   * Fills the remaining space with best fit of pieces possible.
+   *
+   * @param descOrderPieces
+   * @param container
+   * @param wasteIncrement
+   */
+  private static void fillPieceContainerRemainder(
+          PieceList descOrderPieces,
+          PieceContainer container,
+          int wasteIncrement) throws Exception {
+    /// If not piece fits in the free area
+    if (descOrderPieces.areAllBiggerThan(container.getFreeArea())) {
+      /// Do not even try to fit pieces.
+      return;
+    }
+
+    /// Desperdicio máximo permitido
+    int maxAllowedWaste = 0;
+    /// Keep increasing allowed waste until we find pieces that waste the 
+    /// allowed amount or the waste has reached its max (free area).
+    while (!tryFitPieces(descOrderPieces, container, maxAllowedWaste)
+            && maxAllowedWaste <= container.getFreeArea()) {
+      maxAllowedWaste += wasteIncrement;
+    }
+  }
+
+  /**
+   * TODO: Needs testing, this method is high risk. Tries to fit one, two or
+   * three pieces with a given maximum waste.
+   *
+   * @param descOrderPieces from which to select.
+   * @param container in which to fit.
+   * @param maxWaste to lose with piece selection.
+   * @return true if it found and put 1, 2 or 3 pieces that it could fit into
+   * container with maximum waste maxWaste, false otherwise.
+   */
+  private static boolean tryFitPieces(
+          PieceList descOrderPieces,
+          PieceContainer container,
+          int maxWaste) throws Exception {
+
+    /// Complexity: O(n)
+    if (tryFitPiecesRec(descOrderPieces, container, 1, maxWaste)) {
+      return true;
+    }
+    /// Complexity: O(n^2)
+    if (descOrderPieces.size() > 1
+            && tryFitPiecesRec(descOrderPieces, container, 2, maxWaste)) {
+      return true;
+    }
+    /// Complexity: O(n^3)
+    if (descOrderPieces.size() > 2
+            && tryFitPiecesRec(descOrderPieces, container, 3, maxWaste)) {
+      return true;
+    }
+
+    return false;
+  }
+
+  /**
+   * TODO: Needs testing, this method is high risk. Indica si puede o no poner
+   * una piece en el container, dejando un máximo de desperdicio maxWaste. SE
+   * ASUME QUE LA LISTA ESTÁ DADA ORDENADA DE MAYOR A MENOR
+   *
+   * @param descOrderPieces to fit into the container
+   * @param container in which to place the pieces
+   * @param piecesToFit number of pieces to try to place in the container
+   * @param maxWaste maximum allowed free space left in the container after
+   * placing the pieces
+   * @return true if successfully found [piecestoFict] pieces that wasted less
+   * than [maxWaste]
+   * @throws Exception thrown if an error occurs during execution
+   */
+  private static boolean tryFitPiecesRec(
+          PieceList descOrderPieces,
+          PieceContainer container,
+          int piecesToFit,
+          int maxWaste) throws Exception {
+    /// Assert descending order for the first piece (at least)
+    assert (descOrderPieces.get(0).equals(descOrderPieces.getBiggest()));
+
+    // If the are no more pieces to fit in the container.
+    if (piecesToFit == 0) {
+      /// Inform we are done placing pieces
+      return true;
+    }
+
+    for (int i = 0; i < descOrderPieces.size(); i++) {
+      Piece candidate = descOrderPieces.get(i);
+
+      /// If the area(candidate) + area(biggest pieces) leaves too much waste
+      if (container.getFreeArea()
+              - candidate.getArea()
+              - descOrderPieces.getAreaOfFirst(piecesToFit - 1) > maxWaste) {
+        /// Stop because the rest of the pieces will also leave too much waste.
+        break;
+      }
+
+      /// If area(smallest pieces) + area(current candidate) does not fit
+      if (descOrderPieces.getAreaOfLast(piecesToFit - 1)
+              + candidate.getArea() > container.getFreeArea()) {
+        /// Try with a smaller candidate
+        continue;
+      }
+
+      /// If can place the current piece without colisioning
+      if (tryPlaceInBottomLeft(container, candidate)) {
+        /// Place the piece
+        container.putPiece(candidate);
+        /// Remove the piece, so a recursive call won't reconsider it
+        descOrderPieces.remove(candidate);
+        /// If successful at fitting the remaining pieces
+        if (tryFitPiecesRec(
+                descOrderPieces,
+                container,
+                piecesToFit - 1, maxWaste)) {
+          /// Successfully placed candidate piece and remaining pieces!
+          return true;
+        } else {/// Unsuccessful at fitting the remaining pieces.          
+          /// Remove the candidate from the container.
+          container.removePiece(candidate);
+          /// Re-insert the candidate to the list in the order it was.
+          descOrderPieces.insertAt(i, candidate);
+        }
+      }
+    }
+    /// Could not fit any pieces
+    return false;
+  }
+
+  /**
+   * Opens a new container meant to be used to add more pieces to it.
+   *
+   * @param containers
+   * @param containerWidth
+   * @param containerHeight
+   * @return the newly added PieceContainer
+   */
+  static PieceContainer openNewPieceContainer(
+          List<PieceContainer> containers,
+          int containerWidth,
+          int containerHeight) {
+    PieceContainer container = new PieceContainer(
+            containerWidth, containerHeight);
+    containers.add(container);
+
+    return container;
   }
 
   /**
@@ -115,182 +295,5 @@ public class Heuristica {
 
     /// Si no reacomoda la piece (si no cambió de posición)
     return totalHorDistance > 0 || totalVertDistance > 0;
-  }
-
-  /**
-   * Tries to fit one, two or three pieces with a given maximum waste.
-   *
-   * @param descOrderPieces from which to select.
-   * @param container in which to fit.
-   * @param maxWaste to lose with piece selection.
-   * @return true if it found and put 1, 2 or 3 pieces that it could fit into
-   * container with maximum waste maxWaste, false otherwise.
-   */
-  private static boolean tryFitPieces(
-          PieceList descOrderPieces,
-          PieceContainer container,
-          int maxWaste) throws Exception {
-
-    if (tryFitPiecesRec(descOrderPieces, container, 1, maxWaste)) {
-      return true;
-    }
-
-    if (descOrderPieces.size() > 1
-            && tryFitPiecesRec(descOrderPieces, container, 2, maxWaste)) {
-      return true;
-    }
-
-    if (descOrderPieces.size() > 2
-            && tryFitPiecesRec(descOrderPieces, container, 3, maxWaste)) {
-      return true;
-    }
-
-    return false;
-  }
-
-  /**
-   * TODO: Needs testing, this method is high risk. Indica si puede o no poner
-   * una piece en el container, dejando un máximo de desperdicio maxWaste. SE
-   * ASUME QUE LA LISTA ESTÁ DADA ORDENADA DE MAYOR A MENOR
-   */
-  private static boolean tryFitPiecesRec(
-          PieceList descOrderPieces,
-          PieceContainer container,
-          int piecesToFit,
-          int maxWaste) throws Exception {
-    /// Assert descending order for the first piece (at least)
-    assert (descOrderPieces.get(0).equals(descOrderPieces.getBiggest()));
-
-    if (piecesToFit == 0) {
-      // Done placing pieces
-      return true;
-    }
-
-    int bigPiecesArea = descOrderPieces.getAreaOfFirst(piecesToFit - 1);
-    int smallPiecesArea = descOrderPieces.getAreaOfLast(piecesToFit - 1);
-
-    for (int i = 0; i < descOrderPieces.size(); i++) {
-      Piece candidate = descOrderPieces.get(i);
-
-      /// If the area(candidate) + area(biggest pieces) leaves too much waste
-
-      if (container.getFreeArea()
-              - candidate.getArea()
-              - bigPiecesArea > maxWaste) {
-        /// Stop because the rest of the pieces will also leave too much waste.
-        break;
-      }
-
-      /// If area(current candidate) + area(smallest pieces) does not fit
-      if (container.getFreeArea() < candidate.getArea() + smallPiecesArea) {
-        /// Try with a smaller piece
-        continue;
-      }
-
-      /// If can place the current piece without colisioning
-      if (tryPlaceInBottomLeft(container, candidate)) {
-        /// Place the piece
-        container.putPiece(candidate);
-        descOrderPieces.remove(candidate);
-        /// If successful at fitting the remaining pieces
-        if (tryFitPiecesRec(
-                descOrderPieces,
-                container,
-                piecesToFit - 1, maxWaste)) {
-          /// Could place candidate piece!
-          return true;
-        } else {
-          /// Unsuccessful at fitting the candidate with the remaining pieces.
-          /// Remove the candidate from the container.
-          container.removePiece(candidate);
-          /// Re-insert in the piece list in the same order it previously was.
-          descOrderPieces.insertAt(i, candidate);
-        }
-      }
-    }
-    /// Could not fit any pieces
-    return false;
-  }
-
-  /**
-   * Opens a new container meant to be used to add more pieces to it.
-   *
-   * @param containers
-   * @param containerWidth
-   * @param containerHeight
-   * @return the newly added PieceContainer
-   */
-  PieceContainer openNewPieceContainer(
-          List<PieceContainer> containers,
-          int containerWidth,
-          int containerHeight) {
-    PieceContainer container = new PieceContainer(
-            containerWidth, containerHeight);
-    containers.add(container);
-
-    return container;
-  }
-
-  /**
-   * Fills the container with the minimum pieces to fill the initialCapacity
-   *
-   * @param inputPieces
-   * @param container
-   * @param initialCapacity
-   */
-  private void placeWithInitialCapacity(
-          PieceList inputPieces,
-          PieceContainer container,
-          double initialCapacity) throws Exception {
-    /// Recorre de mayor a menor, dado que pieces está en orden DESC
-    for (Piece piece : inputPieces) {
-      // initialCapacity = 1/4 o 1/3
-      if (container.getUsedArea() > container.getArea() * initialCapacity) {
-        break;
-      }
-
-      if (piece.getArea() <= container.getFreeArea()) {
-        /* *
-         * true o false, dependiendo si se puede acomodar la pieza.                        
-         */
-        if (tryPlaceInBottomLeft(container, piece)) {
-          container.putPiece(piece);
-        }
-      }
-    }
-
-    /// Remove all pieces put in the container.
-    for (Piece piece : container) {
-      inputPieces.remove(piece);
-    }
-  }
-
-  /**
-   * Fills the remaining space with best fit of pieces possible.
-   *
-   * @param descOrderPieces
-   * @param container
-   * @param wasteIncrement
-   */
-  private void fillPieceContainerRemainder(
-          PieceList descOrderPieces,
-          PieceContainer container,
-          int wasteIncrement) throws Exception {
-    if (descOrderPieces.areAllBiggerThan(container.getFreeArea())) {
-      /**
-       * Si por area libre, ya no cabe ninguna piece, se pasa al otro
-       * contenedor, en caso de haber.
-       */
-      return;
-    }
-
-    /// Desperdicio máximo permitido
-    int maxAllowedWaste = 0;
-    /// Keep increasing allowed waste until we find pieces that waste the 
-    /// allowed amount or the waste has reached its max (free area).
-    while (!tryFitPieces(descOrderPieces, container, maxAllowedWaste)
-            && maxAllowedWaste <= container.getFreeArea()) {
-      maxAllowedWaste += wasteIncrement;
-    }
   }
 }
